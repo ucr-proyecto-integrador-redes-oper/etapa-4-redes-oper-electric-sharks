cd5bf33674da1d8046e21cb9784b0e9ecb6be5c2
Roy Munoz <royamunoz@gmail.com>
2019-06-28 15:36:07 -0600

    Avance en blues

--- blue.cpp
+++ blue.cpp
@@ -1,14 +1,10 @@
 
 #include "blue.h"
 
-Blue::Blue()
-{
-
-}
-
 Blue::~Blue()
 {
-    //dtor
+    sem_destroy(&this->InBufferSem);
+	sem_destroy(&this->OutBufferSem);
 }
 /** \brief Agrega vecinos al mapa y sus ip al vector
  *
@@ -17,6 +13,27 @@ Blue::~Blue()
  * \return
  *
  */
+
+void *Blue::sender(Blue* blue){
+}
+
+void *Blue::processer(Blue* blue){
+}
+
+void *Blue::sender(Blue* blue){
+}
+
+void *Blue::senderHelper(void *context){
+    return ((Blue *)context)->sender((Blue*) context);
+}
+
+void *Blue::processerHelper(void *context){
+    return ((Blue *)context)->processer((Blue*) context);
+}
+
+void *Blue::receiverHelper(void *context){
+    return ((Blue *)context)->receiver((Blue*) context);
+}
 
 void Blue::neighbors(unsigned int IP,unsigned short int puerto){
    //Mapa de puerto con ip de cada vecino
@@ -34,6 +51,32 @@ void Blue::neighbors(unsigned int IP,unsigned short int puerto){
  *
  */
 
+int Blue::validateIP(char* ip)
+{
+	char ipBuffer[IP_LEN];
+	return (inet_pton(AF_INET, ip, ipBuffer) == 1 ? 1 : 0);
+}
+
+///Funcion que verifica la cantidad de datos por consola y los convierte de strings a los datos de la clase Orange
+void get_args(int &orangeIP, unsigned short int &orangePort, int argc, char ** argv){
+    switch(argc){
+		case 1:
+        case 2:
+			perror("Número de argumentos menor al requerido\n");
+			printf("Formato: %s <ip nodo naranja para solicitar asignacion> <puerto nodo naranja para solicitar asignacion>\n", argv[0]);
+            exit(EXIT_FAILURE);
+        default:
+
+            if((id = stoi(argv[1])) == 0 || (orangeInPort = (unsigned short) stoi(argv[2], NULL)) == 0  || \
+            (orangeOutPort = (unsigned short) stoi(argv[3], NULL)) == 0){ // Si el argumento no es un número válido el programa se cae aquí
+				perror("Invalid argument!\n");
+				printf("Format: %s <port>\n", argv[0]);
+				exit(EXIT_FAILURE);
+            }
+    }
+}
+
+/*
 void Blue::sendChunk(char chunk[1024]){
     int num_Neighbors = ports_Neighbors.size();
     int pointer_vector = 0;
@@ -45,4 +88,38 @@ void Blue::sendChunk(char chunk[1024]){
       pointer_vector = 0;
       //si tiene espacio se deja el chunk porque ya lo envió a todos los vecinos
     }
+}*/
+
+void Blue::putInSendQueue(Blue* blue, Packet* p, int direction)
+{	
+	PacketEntry* newPacket = (PacketEntry*) calloc(1, sizeof(PacketEntry));
+	newPacket->packet = p;
+	newPacket->sendTo = direction;
+	pthread_mutex_lock(&blue->semOut);
+	blue->privateOutBuffer.push(newPacket);
+	pthread_mutex_unlock(&blue->semOut);
+	sem_post(&blue->OutBufferSem);
+}
+
+Blue::Blue()
+{
+}
+
+int main(int argc, char* argv[]){
+    pthread_t receiver;
+    pthread_t processer;
+    pthread_t sender;
+
+    unsigned short int orangePort;
+    
+    Blue blueNode();
+
+    int resReceiver = pthread_create(&receiver, NULL, &Blue::receiverHelper, &blueNode);
+
+
+
+    /*Nunca hacen exit*/
+	pthread_join(receiver, (void**) nullptr);
+	pthread_join(processer, (void**) nullptr);
+	pthread_join(sender, (void**) nullptr);
 }
--- blue.h
+++ blue.h
@@ -11,6 +11,12 @@
 #include <pthread.h>
 #include <queue>
 #include <map>
+#include <semaphore.h>
+
+#define BUF_SIZE 1024
+#define IP_LEN 16
+#define ORANGE_PORT 13000
+
 
 using namespace std;
 
@@ -22,6 +28,14 @@ class Blue{
         queue<PacketEntry*> sharedOutBuffer;
         queue<PacketEntry*> blueRequests;
     
+        pthread_mutex_t semIn;
+        pthread_mutex_t semOut;
+
+        sem_t InBufferSem;
+        sem_t OutBufferSem;
+
+        void putInSendQueue(Blue* blue, Packet* p, int direction);
+
         map <unsigned int, unsigned short int> mapNeighbors;
         //map <unsigned int, unsigned short int>::iterator i;
         vector<unsigned int> ports_Neighbors;
@@ -30,8 +44,20 @@ class Blue{
         void neighbors(unsigned int,unsigned short int);
         void sendChunk(char []);
 
+        static void * receiverHelper(void *context);
+        static void * processerHelper(void *context);
+        static void * senderHelper(void *context);
+
+
+        void * receiver(Blue*);
+        void * processer(Blue*);
+        void * sender(Blue*);
+
     protected:
     private:
+
+        int validateIP(char* ip);
+    
 };
 
 #endif
